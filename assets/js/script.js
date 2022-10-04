4d33c31aaef8f742cbe576fbd91c67f3de6b8701
2macs <adrian.mcneill@outlook.com>
2022-10-04 14:10:14 +0000

    added function to determine game winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ console.log(gameCount.innerHTML);
 for (button of gameButtons){
     button.addEventListener('click',function() {
         let playerChoice = this.getAttribute('data-pick');
-        console.log(playerChoice);
+        
         //start the game
         runGame(playerChoice);
     })
@@ -30,17 +30,70 @@ for (button of gameButtons){
  * This is the main game function, player choice is known. 
  */
 function runGame(playerChoice){
-    //console.log("Game has been called, Player choice is " + playerChoice);
+    
     //change player image to reflect player selection
     playerImage.src = `assets/images/${options[playerChoice]}.PNG`;
     playerImage.alt = options[playerChoice];
 
     //Next get the computer choice, random number between 0 and 4
     let computerChoice = Math.floor(Math.random()*5);
-    console.log(computerChoice);
-
+    
     //next change the computer image to refelct the computer selection
     computerImage.src = `assets/images/${options[computerChoice]}.PNG`;
     computerImage.alt = options[computerChoice];
+
+    // establish who won and deal with ties , call winCalc function   
+    let result = winCalc(playerChoice,computerChoice);
+    console.log(result);
+
+}
+
+/**
+ * This function takes the user selection and computer selection and returns a winner
+ */
+function winCalc(playerChoice,computerChoice){
+    console.log('wincalc has been called,' +playerChoice + ',' + computerChoice);
+    
+    if (playerChoice == computerChoice){ //its a tie
+        return 0;
+    } else if (playerChoice == 0){  // Player selects rock
+        if (computerChoice == 2 || computerChoice == 3){
+            return 1; // Player wins
+        }
+        else{
+            return 2; //Computer wins
+        }
+    } else if (playerChoice == 1){  // Player selects paper
+        if (computerChoice == 0 || computerChoice == 4){
+            return 1; // Player wins
+        }
+        else{
+            return 2; //Computer wins
+        }
+    } else if (playerChoice == 2){  // Player selects scissors
+        if (computerChoice == 1 || computerChoice == 3){
+            return 1; // Player wins
+        }
+        else{
+            return 2; //Computer wins
+        }
+    } else if (playerChoice == 3){  // Player selects lizard
+        if (computerChoice == 4 || computerChoice == 1){
+            return 1; // Player wins
+        }
+        else{
+            return 2; //Computer wins
+        }
+    } else if (playerChoice == 4){  // Player selects lizard
+        if (computerChoice == 0 || computerChoice == 3){
+            return 1; // Player wins
+        }
+        else{
+            return 2; //Computer wins
+        }
+    }
+    else { // problem with game
+        winnerMessage.innerText = "Problem with game, please try again";
+    }
 
 }
